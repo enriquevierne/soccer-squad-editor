5e86e36ac66e340a2c81135180d6a73cb367659f
Enrique Vierne <110237705+enriquevierne@users.noreply.github.com>
2024-04-29 17:45:38 -0300

    add away team mobile is done

--- src/components/Teams.vue
+++ src/components/Teams.vue
@@ -2,49 +2,49 @@
 const AwayPositionsList = [
     {
         position: 'GL',
-        class: 'col-span-3 col-start-4 row-start-10',
+        class: 'col-span-3 col-start-4 row-start-10 md:col-span-1 md:col-start-10 md:row-start-5',
         number: 1,
         color: 'bg-gradient-to-br from-blue-700 to-blue-900 border-blue-950',
         name: 'Maria Joaquina'
     },
     {
         position: 'ZE',
-        class: 'col-span-3 col-start-2 row-start-9',
+        class: 'col-span-3 col-start-2 row-start-9 md:col-span-1 md:col-start-9 md:row-start-7',
         number: 4,
         color: 'bg-gradient-to-br from-blue-700 to-blue-900 border-blue-950',
         name: 'Maria Joaquina'
     },
     {
         position: 'ZD',
-        class: 'col-span-3 col-start-6 row-start-9',
+        class: 'col-span-3 col-start-6 row-start-9 md:col-span-1 md:col-start-9 md:row-start-3',
         number: 3,
         color: 'bg-gradient-to-br from-blue-700 to-blue-900 border-blue-950',
         name: 'Maria Joaquina'
     },
     {
         position: 'VOL',
-        class: 'col-span-3 col-start-4 row-start-8',
+        class: 'col-span-3 col-start-4 row-start-8 md:col-span-1 md:col-start-8 md:row-start-5',
         number: 5,
         color: 'bg-gradient-to-br from-blue-700 to-blue-900 border-blue-950',
         name: 'Maria Joaquina'
     },
     {
         position: 'ME',
-        class: 'col-span-3 col-start-1 row-start-7',
+        class: 'col-span-3 col-start-1 row-start-7 md:col-span-1 md:col-start-7 md:row-start-2',
         number: 10,
         color: 'bg-gradient-to-br from-blue-700 to-blue-900 border-blue-950',
         name: 'Maria Joaquina'
     },
     {
         position: 'MD',
-        class: 'col-span-3 col-start-7 row-start-7',
+        class: 'col-span-3 col-start-7 row-start-7 md:col-span-1 md:col-start-7 md:row-start-8',
         number: 11,
         color: 'bg-gradient-to-br from-blue-700 to-blue-900 border-blue-950',
         name: 'Maria Joaquina'
     },
     {
         position: 'ATA',
-        class: 'col-span-3 col-start-4 row-start-6',
+        class: 'col-span-3 col-start-4 row-start-6 md:col-span-1 md:col-start-6 md:row-start-5',
         number: 9,
         color: 'bg-gradient-to-br from-blue-700 to-blue-900 border-blue-950',
         name: 'Maria Joaquina'
@@ -53,49 +53,49 @@ const AwayPositionsList = [
 const HomePositionsList = [
     {
         position: 'GL',
-        class: 'col-span-3 col-start-4 row-start-1',
+        class: 'col-span-3 col-start-4 row-start-1 md:col-span-1 md:col-start-1 md:row-start-5',
         color: 'bg-gradient-to-br from-gray-700 to-gray-900 border-gray-950',
         number: 1,
         name: 'João Carlos'
     },
     {
         position: 'ZD',
-        class: 'col-span-3 col-start-2 row-start-2',
+        class: 'col-span-3 col-start-2 row-start-2 md:col-span-1 md:col-start-2 md:row-start-6',
         color: 'bg-gradient-to-br from-gray-700 to-gray-900 border-gray-950',
         number: 4,
         name: 'João Carlos'
     },
     {
         position: 'ZE',
-        class: 'col-span-3 col-start-6 row-start-2',
+        class: 'col-span-3 col-start-6 row-start-2 md:col-span-1 md:col-start-2 md:row-start-3',
         color: 'bg-gradient-to-br from-gray-700 to-gray-900 border-gray-950',
         number: 3,
         name: 'João Carlos'
     },
     {
         position: 'VOL',
-        class: 'col-span-3 col-start-4 row-start-3',
+        class: 'col-span-3 col-start-4 row-start-3 md:col-span-1 md:col-start-3 md:row-start-5',
         color: 'bg-gradient-to-br from-gray-700 to-gray-900 border-gray-950',
         number: 5,
         name: 'João Carlos'
     },
     {
         position: 'MD',
-        class: 'col-span-3 col-start-1 row-start-4',
+        class: 'col-span-3 col-start-1 row-start-4 md:col-span-1 md:col-start-4 md:row-start-8',
         color: 'bg-gradient-to-br from-gray-700 to-gray-900 border-gray-950',
         number: 10,
         name: 'João Carlos'
     },
     {
         position: 'ME',
-        class: 'col-span-3 col-start-7 row-start-4',
+        class: 'col-span-3 col-start-7 row-start-4 md:col-span-1 md:col-start-4 md:row-start-1',
         color: 'bg-gradient-to-br from-gray-700 to-gray-900 border-gray-950',
         number: 11,
         name: 'João Carlos'
     },
     {
         position: 'ATA',
-        class: 'col-span-3 col-start-4 row-start-5',
+        class: 'col-span-3 col-start-4 row-start-5 md:col-span-1 md:col-start-5 md:row-start-5',
         color: 'bg-gradient-to-br from-gray-700 to-gray-900 border-gray-950',
         number: 9,
         name: 'João Carlos'
@@ -114,11 +114,11 @@ const HomePositionsList = [
         </li>
         <li v-for="pos in AwayPositionsList" :class="pos.class"
             class="flex flex-col items-center justify-center w-full h-full gap-1 text-gray-100 ">
+            <span class="p-1 text-sm rounded-full" :class="pos.color">{{ pos.name }}</span>
             <div class="flex flex-col items-center p-1 border-2 rounded-full w-fit" :class="pos.color">
                 <span class="text-xs font-semibold">{{ pos.position }} </span>
                 <span>({{ pos.number }})</span>
             </div>
-            <span class="p-1 text-sm rounded-full" :class="pos.color">{{ pos.name }}</span>
         </li>
     </ul>
 </template>
