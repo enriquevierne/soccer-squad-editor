4bdb6695ba0636bfd5682486c655a9452fb4c636
Enrique Vierne <viernee@gmail.com>
2024-04-29 13:00:22 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Menu from './components/Menu.vue';
+</script>
+<template>
+  <main class="w-full min-h-screen bg-gray-100 dark:bg-gray-900">
+    <Menu />
+  </main>
+</template>
---
+++ src/components/Menu.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { HomeIcon } from '@heroicons/vue/24/solid'
+</script>
+<template>
+  <nav class="fixed bottom-0 w-full h-20 border-t-2 border-gray-800 md:top-0 md:border-0 md:shadow-lg ">
+    <div class="m-auto max-w-7xl">
+      <a href=""><HomeIcon class="w-5 dark:fill-gray-100 fill-gray-700" /></a>
+      <a href="">teams</a>
+      <a href="">players</a>
+      <ToggleTheme />
+    </div>
+  </nav>
+</template>
