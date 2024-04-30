4ec9424b894cb7d1d2f9a2fbd23584df0ad63cc2
Enrique Vierne <110237705+enriquevierne@users.noreply.github.com>
2024-04-30 10:30:21 -0300

    add teams with differnet strategies

--- src/components/Teams.vue
+++ src/components/Teams.vue
@@ -2,49 +2,49 @@
 const AwayPositionsList = [
     {
         position: 'GL',
-        class: 'col-span-3 col-start-4 row-start-10 md:col-span-1 md:col-start-10 md:row-start-5',
+        class: 'col-span-1 col-start-5 row-start-10 md:col-span-1 md:col-start-8 md:row-start-6',
         number: 1,
         color: 'bg-gradient-to-br from-blue-700 to-blue-900 border-blue-950',
         name: 'William'
     },
     {
         position: 'ZE',
-        class: 'col-span-3 col-start-2 row-start-9 md:col-span-1 md:col-start-9 md:row-start-7',
+        class: 'col-span-1 col-start-3 row-start-9 md:col-span-1 md:col-start-7 md:row-start-7',
         number: 4,
         color: 'bg-gradient-to-br from-blue-700 to-blue-900 border-blue-950',
         name: 'Lucas'
     },
     {
         position: 'ZD',
-        class: 'col-span-3 col-start-6 row-start-9 md:col-span-1 md:col-start-9 md:row-start-3',
+        class: 'col-span-1 col-start-7 row-start-9 md:col-span-1 md:col-start-7 md:row-start-4',
         number: 3,
         color: 'bg-gradient-to-br from-blue-700 to-blue-900 border-blue-950',
         name: 'Valdemar'
     },
     {
         position: 'VOL',
-        class: 'col-span-3 col-start-4 row-start-8 md:col-span-1 md:col-start-8 md:row-start-5',
+        class: 'col-span-1 col-start-5 row-start-8 md:col-span-1 md:col-start-7 md:row-start-6',
         number: 5,
         color: 'bg-gradient-to-br from-blue-700 to-blue-900 border-blue-950',
         name: 'Eduardo'
     },
     {
         position: 'ME',
-        class: 'col-span-3 col-start-1 row-start-7 md:col-span-1 md:col-start-7 md:row-start-2',
+        class: 'col-span-1 col-start-1 row-start-7 md:col-span-1 md:col-start-6 md:row-start-3',
         number: 10,
         color: 'bg-gradient-to-br from-blue-700 to-blue-900 border-blue-950',
         name: 'Macarrão'
     },
     {
         position: 'MD',
-        class: 'col-span-3 col-start-7 row-start-7 md:col-span-1 md:col-start-7 md:row-start-8',
+        class: 'col-span-1 col-start-9 row-start-7 md:col-span-1 md:col-start-6 md:row-start-9',
         number: 11,
         color: 'bg-gradient-to-br from-blue-700 to-blue-900 border-blue-950',
         name: 'Maicão'
     },
     {
         position: 'ATA',
-        class: 'col-span-3 col-start-4 row-start-6 md:col-span-1 md:col-start-6 md:row-start-5',
+        class: 'col-span-1 col-start-5 row-start-6 md:col-span-1 md:col-start-6 md:row-start-6',
         number: 9,
         color: 'bg-gradient-to-br from-blue-700 to-blue-900 border-blue-950',
         name: 'Jorge'
@@ -53,49 +53,49 @@ const AwayPositionsList = [
 const HomePositionsList = [
     {
         position: 'GL',
-        class: 'col-span-3 col-start-4 row-start-1 md:col-span-1 md:col-start-1 md:row-start-5',
+        class: 'col-span-1 col-start-5 row-start-1 md:col-span-1 md:col-start-2 md:row-start-6',
         color: 'bg-gradient-to-br from-gray-700 to-gray-900 border-gray-950',
         number: 1,
         name: 'Murilo'
     },
     {
         position: 'ZC',
-        class: 'col-span-3 col-start-2 row-start-2 md:col-span-1 md:col-start-2 md:row-start-5',
+        class: 'col-span-1 col-start-5 row-start-2 md:col-span-1 md:col-start-3 md:row-start-6',
         color: 'bg-gradient-to-br from-gray-700 to-gray-900 border-gray-950',
         number: 4,
         name: 'Ivan'
     },
     {
-        position: 'ZD',
-        class: 'col-span-3 col-start-7 row-start-4 md:col-span-1 md:col-start-3 md:row-start-2',
+        position: 'ALA',
+        class: 'col-span-1 col-start-3 row-start-3 md:col-span-1 md:col-start-3 md:row-start-8',
         color: 'bg-gradient-to-br from-gray-700 to-gray-900 border-gray-950',
         number: 11,
         name: 'Matheus'
     },
     {
-        position: 'ZE',
-        class: 'col-span-3 col-start-6 row-start-2 md:col-span-1 md:col-start-3 md:row-start-7',
+        position: 'ALA',
+        class: 'col-span-1 col-start-7 row-start-3 md:col-span-1 md:col-start-3 md:row-start-3',
         color: 'bg-gradient-to-br from-gray-700 to-gray-900 border-gray-950',
         number: 3,
         name: 'Digão'
     },
     {
         position: 'ME',
-        class: 'col-span-3 col-start-4 row-start-3 md:col-span-1 md:col-start-4 md:row-start-6',
+        class: 'col-span-1 col-start-8 row-start-4 md:col-span-1 md:col-start-4 md:row-start-4',
         color: 'bg-gradient-to-br from-gray-700 to-gray-900 border-gray-950',
         number: 5,
         name: 'Zinho'
     },
     {
         position: 'MD',
-        class: 'col-span-3 col-start-1 row-start-4 md:col-span-1 md:col-start-4 md:row-start-4',
+        class: 'col-span-1 col-start-2 row-start-4 md:col-span-1 md:col-start-4 md:row-start-7',
         color: 'bg-gradient-to-br from-gray-700 to-gray-900 border-gray-950',
         number: 10,
         name: 'Gustavinho'
     },
     {
         position: 'ATA',
-        class: 'col-span-3 col-start-4 row-start-5 md:col-span-1 md:col-start-5 md:row-start-5',
+        class: 'col-span-1 col-start-5 row-start-5 md:col-span-1 md:col-start-5 md:row-start-6',
         color: 'bg-gradient-to-br from-gray-700 to-gray-900 border-gray-950',
         number: 9,
         name: 'Enrique'
